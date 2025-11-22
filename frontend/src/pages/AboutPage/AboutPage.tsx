81accec75b5faa7aa94c0f36db8430abdedb4447
acuraels <daniil.rib@mail.ru>
2025-11-22 13:35:30 +0500

    feat: base-structure-created, header, footer

---
+++ frontend/src/pages/AboutPage/AboutPage.tsx
@@ -0,0 +1,18 @@
+import Header from '../../components/Header/Header';
+import Footer from '../../components/Footer/Footer';
+
+import "./AboutPage.css";
+
+const AboutPage = () => {
+    return (
+        <>
+            <Header />
+            <main className='home-container'>
+                <h1 className='home-title'>ЕЧБ библиотеки №14</h1>
+            </main>
+            <Footer />
+        </>
+    );
+};
+
+export default AboutPage;
