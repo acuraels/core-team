b2dcd8aeb94d4ebf3518762bfadac27f179ef4ef
Aleksey <skvch.al@gmail.com>
2025-11-23 04:14:56 +0500

    fix(ai): configure cors

--- ai/src/__main__.py
+++ ai/src/__main__.py
@@ -1,6 +1,7 @@
 import uvicorn
 from dotenv import load_dotenv
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from src.api import router as main_router
 
@@ -10,6 +11,17 @@ app = FastAPI(title="Book Recommender AI")
 app.include_router(
     main_router,
     prefix="/api/v1/recommend",
+)
+
+# Настройка CORS
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 if __name__ == "__main__":
